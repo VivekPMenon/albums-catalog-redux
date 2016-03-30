a23e4f09b557580fbbe661b2cf731c2e0bc418b0
Vivek Menon <vivekpmenon@gmail.com>
2016-03-30 14:12:03 -0400

    Create interface-album.ts

---
+++ src/album-catalog-actions/interface-album.ts
@@ -0,0 +1,14 @@
+interface IAlbum{
+    id?:number;
+    artist?:string;
+    title?:string;
+    year?: string;
+    country?: string;
+    downloads?:number;
+    genre?:string;
+    description?:string;
+}
+
+export {
+    IAlbum
+}
