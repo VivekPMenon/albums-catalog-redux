e43d7d24c90c4df739ee7198f5f5d8cd0814a65d
Vivek Menon <vivekpmenon@gmail.com>
2016-03-30 14:48:39 -0400

    ading middleware for logger

--- src/album-catalog-store/album-catalog.store.ts
+++ src/album-catalog-store/album-catalog.store.ts
@@ -1,6 +1,13 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
 import mainReducer from '../album-catalog-reducers/index';
 
-const albumCatalogStore = createStore(mainReducer);
+const logger = new createLogger();
+const albumCatalogStore = createStore(
+    mainReducer,
+    applyMiddleware(thunk, promise, logger)
+);
 
 export default albumCatalogStore;
